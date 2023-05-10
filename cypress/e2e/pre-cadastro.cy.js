914cec13d6debbe88c48004972038174318dadc9
Leonardo <leodj2000.ld@gmail.com>
2023-05-10 17:02:02 -0300

    usando massa de dados

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker';
 describe('Funcionalidade pré cadastro', () => {
 
     before(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     })
 
     afterEach(() => {
